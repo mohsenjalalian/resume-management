c7a24243287cbbe033db3a3fb1b9106b8f054ddf
mohsenjalalian <jalalianmohsen@gmail.com>
2020-01-18 00:46:51 +0330

    add gorm model

--- src/models/models.go
+++ src/models/models.go
@@ -1,6 +1,11 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type Resume struct {
+	gorm.Model
 	Title   string `json:"title"`
 	Path    string `json:"path"`
 	Content string `json:"content"`
